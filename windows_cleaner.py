1a3eb25181fb75ad8100d48cf525f11d136fb9c1
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-04-03 01:14:25 +0100

    many optimisations

--- windows_cleaner.py
+++ windows_cleaner.py
@@ -2,6 +2,10 @@ import os
 import subprocess
 import winreg
 import shutil
+import urllib.request
+import zipfile
+import wmi
+
 
 # Set console window title
 os.system("title Windows_Optimisation_Pack Cleaner | /u00A9 unofficialdxnny")
@@ -109,3 +113,85 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+hash = {}
+ScriptFolder = os.path.join(os.environ['TEMP'], 'Windows_Optimisation_Pack')
+try:
+    winreg.CreateKey(winreg.HKEY_LOCAL_MACHINE, 'SOFTWARE\\Windows_Optimisation_Pack')
+except:
+    pass
+WindowsVersion = wmi.WMI().Win32_OperatingSystem()[0].Caption
+InstalledSoftware = [item.DisplayName for item in winreg.QueryInfoKey(winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r'SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall'))[0]]
+if not os.path.exists(ScriptFolder):
+    os.mkdir(ScriptFolder)
+
+
+def WindowsTweaks_Services():
+    subprocess.run(["Stop-Service", "WpcMonSvc"])
+    subprocess.run(["Stop-Service", "SharedRealitySvc"])
+    subprocess.run(["Stop-Service", "Fax"])
+    subprocess.run(["Stop-Service", "autotimesvc"])
+    subprocess.run(["Stop-Service", "wisvc"])
+    subprocess.run(["Stop-Service", "SDRSVC"])
+    subprocess.run(["Stop-Service", "MixedRealityOpenXRSvc"])
+    subprocess.run(["Stop-Service", "WalletService"])
+    subprocess.run(["Stop-Service", "SmsRouter"])
+    subprocess.run(["Stop-Service", "SharedAccess"])
+    subprocess.run(["Stop-Service", "MapsBroker"])
+    subprocess.run(["Stop-Service", "PhoneSvc"])
+    subprocess.run(["Stop-Service", "ScDeviceEnum"])
+    subprocess.run(["Stop-Service", "icssvc"])
+    subprocess.run(["Stop-Service", "edgeupdatem"])
+    subprocess.run(["Stop-Service", "edgeupdate"])
+    subprocess.run(["Stop-Service", "MicrosoftEdgeElevationService"])
+    subprocess.run(["Stop-Service", "RetailDemo"])
+    subprocess.run(["Stop-Service", "MessagingService"])
+    subprocess.run(["Stop-Service", "PimIndexMaintenanceSvc"])
+    subprocess.run(["Stop-Service", "OneSyncSvc"])
+    subprocess.run(["Stop-Service", "UnistoreSvc"])
+    subprocess.run(["Stop-Service", "DiagTrack"])
+    subprocess.run(["Stop-Service", "dmwappushservice"])
+    subprocess.run(["Stop-Service", "diagnosticshub.standardcollector.service"])
+    subprocess.run(["Stop-Service", "diagsvc"])
+    subprocess.run(["Stop-Service", "WerSvc"])
+    subprocess.run(["Stop-Service", "wercplsupport"])
+    subprocess.run(["Set-Service", "WpcMonSvc", "-StartupType", "Disabled"])
+    subprocess.run(["Set-Service", "SharedRealitySvc", "-StartupType", "Disabled"])
+    subprocess.run(["Set-Service", "Fax", "-StartupType", "Disabled"])
+    subprocess.run(["Set-Service", "autotimesvc", "-StartupType", "Disabled"])
+    subprocess.run(["Set-Service", "wisvc", "-StartupType", "Disabled"])
+    subprocess.run(["Set-Service", "SDRSVC", "-StartupType", "Disabled"])
+    subprocess.run(["Set-Service", "MixedRealityOpenXRSvc", "-StartupType", "Disabled"])
+    subprocess.run(["Set-Service", "WalletService", "-StartupType", "Disabled"])
+    subprocess.run(["Set-Service", "SmsRouter", "-StartupType", "Disabled"])
+    subprocess.run(["Set-Service", "SharedAccess", "-StartupType", "Disabled"])
+    subprocess.run(["Set-Service", "MapsBroker", "-StartupType", "Disabled"])
+    subprocess.run(["Set-Service", "PhoneSvc", "-StartupType", "Disabled"])
+    subprocess.run(["Set-Service", "ScDeviceEnum", "-StartupType", "Disabled"])
+    subprocess.run(["Set-Service", "TabletInputService", "-StartupType", "Disabled"])
+    subprocess.run(["Set-Service", "icssvc", "-StartupType", "Disabled"])
+    subprocess.run(["Set-Service", "edgeupdatem", "-StartupType", "Disabled"])
+    subprocess.run(["Set-Service", "edgeupdate", "-StartupType", "Disabled"])
+    subprocess.run(["Set-Service", "MicrosoftEdgeElevationService", "-StartupType", "Disabled"])
+    
+services = [
+    "RetailDemo",
+    "MessagingService",
+    "PimIndexMaintenanceSvc",
+    "OneSyncSvc",
+    "UnistoreSvc",
+    "DiagTrack",
+    "dmwappushservice",
+    "diagnosticshub.standardcollector.service",
+    "diagsvc",
+    "WerSvc",
+    "wercplsupport"
+]
+
+for service in services:
+    subprocess.run(["sc", "config", service, "start=", "disabled"])
+
+
+
